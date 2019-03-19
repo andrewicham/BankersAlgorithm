4da353b92cf04f44883aab0925399ee4eda96a56
andrewicham <andrewicham@gmail.com>
2019-03-19 00:48:08 -0400

    added command line argument functionality (argv)

--- BankersAlg.c
+++ BankersAlg.c
@@ -22,7 +22,8 @@ int releaseResources(int customerNum, int release[]);
 
 
 /* This will dynamically allocate a two dimensional array, with the user defined number of customers (processes) being one dimension, and the statically
- defined number of resources being the other. *** is used because the address of a 2d array is being passed into function */
+ defined number of resources being the other. *** is used because the address of a 2d array is being passed into function, so that a copy of the 2d array is not 
+ created instead */
 void setMatrixSize(int ***arr, int size){
     int i;
     *arr = malloc(size * sizeof(int *));
@@ -35,7 +36,7 @@ void setMatrixSize(int ***arr, int size){
     }
     
 }
-
+/* populates the matrix with random values between 1 and 100 */
 void populateMatrix(int** arr, int size){
     int i;
     int j;
@@ -61,27 +62,36 @@ void printMatrix(int** arr, int size)
 
 /* The runner function passed into each instance of pthread init */
 void* processJob(void *arg){
+    //locks the mutex, so that other processes may not have access to the critical section
     pthread_mutex_lock(&lock);
 
     //code for the thread to execute goes here
 
+    //unlocks the mutex so that another process may operate on the critical section
     pthread_mutex_unlock(&lock);
-
     pthread_exit(0);
 }
 
 int main(int argc, char *argv[]){
     int size;
-    int available[NUM_RESOURCES] = {2, 3, 10};
-    
-    printf("Enter customer number: ");
+    //initializing mutex
+    pthread_mutex_init(&lock, NULL);
+
+    /* takes in the number of resources as command line arguments and stores them in the available array. 
+    only will work for commands that have NUM_RESOURCES numerical arguments */
+    for(int i = 0; i < NUM_RESOURCES; i++){
+        long convert = strtol(argv[i+1], NULL, 10);
+        available[i] = convert;
+    }
+    printf("Enter the number of customers: ");
     scanf("%d", &size);
     
     //setting the maximum array size and populating with random numbers
     setMatrixSize(&maximum, size);
     populateMatrix(maximum, size);
     
-
+    //destroys the initialized mutex
+    pthread_mutex_destroy(&lock);
     free(maximum);
     return 0;
 }
