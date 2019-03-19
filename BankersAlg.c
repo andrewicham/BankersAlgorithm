580b2bed24c3e23d04bf9518adb5284ed2c3ea78
andrewicham <andrewicham@gmail.com>
2019-03-18 20:49:06 -0400

    added mutex lock

--- BankersAlg.c
+++ BankersAlg.c
@@ -5,23 +5,29 @@
 
 #define NUM_RESOURCES 3
 
-
+/* Arrays were declared globally so that they could be easily accessed by processes */
 int **maximum;
 int available[NUM_RESOURCES];
 int **allocation;
 int **need;
 
-void setArraySize(int ***arr, int size);
-void populateArray(int **arr, int size);
+pthread_mutex_t lock;
+
+void setMatrixSize(int ***arr, int size);
+void populateMatrix(int **arr, int size);
 void printMatrix(int **arr, int size);
+void* processJob(void *arg);
+int requestResources(int customerNum, int request[]);
+int releaseResources(int customerNum, int release[]);
+
 
 /* This will dynamically allocate a two dimensional array, with the user defined number of customers (processes) being one dimension, and the statically
  defined number of resources being the other. *** is used because the address of a 2d array is being passed into function */
-void setArraySize(int ***arr, int size){
+void setMatrixSize(int ***arr, int size){
     int i;
     *arr = malloc(size * sizeof(int *));
     if(*arr == NULL){
-        fprintf(stderr, "no memory\n");
+        fprintf(stderr, "no memory available\n");
         return;
     }
     for(i = 0; i < size; i++){
@@ -30,7 +36,7 @@ void setArraySize(int ***arr, int size){
     
 }
 
-void populateArray(int** arr, int size){
+void populateMatrix(int** arr, int size){
     int i;
     int j;
     srand(time(0));
@@ -51,17 +57,31 @@ void printMatrix(int** arr, int size)
             printf("arr[i][j]: %d \n", arr[i][j]);
         }
     }
-    
 }
+
+/* The runner function passed into each instance of pthread init */
+void* processJob(void *arg){
+    pthread_mutex_lock(&lock);
+
+    //code for the thread to execute goes here
+
+    pthread_mutex_unlock(&lock);
+
+    pthread_exit(0);
+}
+
 int main(int argc, char *argv[]){
     int size;
     int available[NUM_RESOURCES] = {2, 3, 10};
     
-    printf("Enter size: ");
+    printf("Enter customer number: ");
     scanf("%d", &size);
-    setArraySize(&maximum, size);
-    populateArray(maximum, size);
-    printMatrix(maximum, size);
+    
+    //setting the maximum array size and populating with random numbers
+    setMatrixSize(&maximum, size);
+    populateMatrix(maximum, size);
+    
+
     free(maximum);
     return 0;
 }
