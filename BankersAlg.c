4caa8462f45e54d2ce3447db29aeee7d987af19c
andrewicham <andrewicham@gmail.com>
2019-03-18 20:18:43 -0400

    corrected the memory allocation by passing the 2D array by reference

--- BankersAlg.c
+++ BankersAlg.c
@@ -2,60 +2,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//can be any values >= 0
+
 #define NUM_RESOURCES 3
 
+
+int **maximum;
 int available[NUM_RESOURCES];
-int** maximum = NULL;
-int* allocation = NULL;
-int* need = NULL;
+int **allocation;
+int **need;
 
-void setArraySize(int **arr, unsigned int size);
-void populateArray(int **arr, unsigned int size);
-void freeArray(int *arr);
+void setArraySize(int ***arr, int size);
+void populateArray(int **arr, int size);
+void printMatrix(int **arr, int size);
 
-/* This will dynamically allocate a two dimensional array, with the user defined number of customers being one dimension, and the statically 
-defined number of resources being the other */
-void setArraySize(int **arr, unsigned int size){
-    arr = malloc(sizeof(int[size][NUM_RESOURCES]));
-    printf("Size: %u\n", size);
-}
-
-void populateArray(int **arr, unsigned int size){
+/* This will dynamically allocate a two dimensional array, with the user defined number of customers (processes) being one dimension, and the statically
+ defined number of resources being the other. *** is used because the address of a 2d array is being passed into function */
+void setArraySize(int ***arr, int size){
     int i;
-    int j;
-    
-    //current time used as the seed for the random number generator
-    srand(time(0));
-    
+    *arr = malloc(size * sizeof(int *));
+    if(*arr == NULL){
+        fprintf(stderr, "no memory\n");
+        return;
+    }
     for(i = 0; i < size; i++){
-        for (j = 0; j < NUM_RESOURCES; j++)
-        {   
-            
-        }
+        (*arr)[i] = malloc(NUM_RESOURCES * sizeof(int));
     }
     
 }
 
-void printArray(int** arr, unsigned int size)
+void populateArray(int** arr, int size){
+    int i;
+    int j;
+    srand(time(0));
+    for(i = 0; i < size; i++){
+        for(j = 0; j < NUM_RESOURCES; j++){
+            arr[i][j] = rand() % 100;
+        }
+    }
+}
+
+void printMatrix(int** arr, int size)
 {
     int i;
     int j;
-    for(i = 0; i < NUM_RESOURCES; i++){
-        for(j = 0; j < size; j++)
+    for(i = 0; i < size; i++){
+        for(j = 0; j < NUM_RESOURCES; j++)
         {
-            printf("Array Value: %d\n", *arr[j]);
+            printf("arr[i][j]: %d \n", arr[i][j]);
         }
     }
     
 }
-int main(){
-    unsigned int size;
+int main(int argc, char *argv[]){
+    int size;
+    int available[NUM_RESOURCES] = {2, 3, 10};
+    
     printf("Enter size: ");
-    scanf("%u", &size);
-    setArraySize(maximum, size);
+    scanf("%d", &size);
+    setArraySize(&maximum, size);
     populateArray(maximum, size);
-    //printArray(maximum, size);
+    printMatrix(maximum, size);
     free(maximum);
     return 0;
 }
