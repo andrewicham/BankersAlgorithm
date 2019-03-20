7df87e65f10b24d9a56b126e82b081f8e8aea777
andrewicham <andrewicham@gmail.com>
2019-03-20 12:48:06 -0400

    added more to safety

--- BankersAlg.c
+++ BankersAlg.c
@@ -12,18 +12,27 @@ int **allocation;
 int **need;
 
 pthread_mutex_t lock;
-
+void setArraySize(int **arr, int size);
 void setMatrixSize(int ***arr, int size);
 void populateMatrix(int **arr, int size);
 void printMatrix(int **arr, int size);
-void* processJob(void *arg);
+void* customer(void *arg);
 int requestResources(int customerNum, int request[]);
 int releaseResources(int customerNum, int release[]);
+int safety(int number_customers);
 
+struct{
+    int number_customers;
+}customerArgs;
 
 /* This will dynamically allocate a two dimensional array, with the user defined number of customers (processes) being one dimension, and the statically
  defined number of resources being the other. *** is used because the address of a 2d array is being passed into function, so that a copy of the 2d array is not 
  created instead */
+
+void setArraySize(int **arr, int size){
+    int i;
+    *arr = malloc(size * sizeof(int));
+}
 void setMatrixSize(int ***arr, int size){
     int i;
     *arr = malloc(size * sizeof(int *));
@@ -36,7 +45,7 @@ void setMatrixSize(int ***arr, int size){
     }
     
 }
-/* populates the matrix with random values between 1 and 100 */
+/* populates the 2d array with random values between 1 and 100 */
 void populateMatrix(int** arr, int size){
     int i;
     int j;
@@ -47,7 +56,7 @@ void populateMatrix(int** arr, int size){
         }
     }
 }
-
+/* utility function to print any 2d arrays */
 void printMatrix(int** arr, int size)
 {
     int i;
@@ -61,9 +70,13 @@ void printMatrix(int** arr, int size)
 }
 
 /* The runner function passed into each instance of pthread init */
-void* processJob(void *arg){
+void* customer(void *arg){
     //locks the mutex, so that other processes may not have access to the critical section
     pthread_mutex_lock(&lock);
+
+    int request[NUM_RESOURCES];
+    
+    
 
     //code for the thread to execute goes here
 
@@ -72,8 +85,76 @@ void* processJob(void *arg){
     pthread_exit(0);
 }
 
+int requestResources(int customerNum, int request[]){
+    int i;
+    for(i = 0; i < NUM_RESOURCES; i++){
+        need[customerNum][i] = need[customerNum][i] - request[i];
+        allocation[customerNum][i] += request[i];
+        available[i] = available[i] - request[i];
+    }
+    return 0;
+}
+
+int releaseResources(int customerNum, int release[]){
+    return 0;
+}
+
+int safety(number_customers){
+    int m = NUM_RESOURCES;
+    int n = number_customers;
+    int* finish;
+    int* work;
+    int i;
+    int j;
+    int k;
+    int flag = 0;
+    //since the number of customers not known at run time, must use dynamic array
+    setArraySize(&finish, n);
+    setArraySize(&work, m);
+    //sets all values in finish array initially to false
+    for(i = 0; i < n; i++){
+        finish[i] = 0;
+    }
+    for(i = 0; i < m; i++){
+        work[i] = available[i];
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            if(need[i][j] <= work[i] && finish[i] == 0){
+                work[i] = work[i] + allocation[i][j];
+                finish[i] = 1;
+            }
+        }
+    }
+    for(i = 0; i < m; i++){
+        printf("Work: %d\n", work[i]);
+    }
+    for(i = 0; i < n; i++){
+        printf("Finish: %d\n", finish[i]);
+    }
+    //if any processes are false, returns 1 to say that the process isn't safe
+    for(i = 0; i < n; i++){
+        if(finish[i] == 0){
+            free(finish);
+            free(work);
+            return 1;
+        }
+    }
+    //frees the dynamically allocated finish array
+    free(finish);
+    free(work);
+    return 0;
+}
+
 int main(int argc, char *argv[]){
-    int size;
+    //the instructions call for the number of customers to vary, so implemented as a user defined value and allocated at runtime
+    int number_customers;
+    printf("Enter the number of customers: ");
+    scanf("%d", &number_customers);
+
+    pthread_t *tid = malloc(sizeof(pthread_t) * number_customers);
+    int *pid = malloc(sizeof(int) * number_customers);
+
     //initializing mutex
     pthread_mutex_init(&lock, NULL);
 
@@ -83,15 +164,26 @@ int main(int argc, char *argv[]){
         long convert = strtol(argv[i+1], NULL, 10);
         available[i] = convert;
     }
-    printf("Enter the number of customers: ");
-    scanf("%d", &size);
     
-    //setting the maximum array size and populating with random numbers
-    setMatrixSize(&maximum, size);
-    populateMatrix(maximum, size);
     
+    //setting the maximum matrix size and populating with random numbers 
+    setMatrixSize(&need, number_customers);
+    populateMatrix(need, number_customers);
+    //printMatrix(need, number_customers);
+    setMatrixSize(&allocation, number_customers);
+    populateMatrix(allocation, number_customers);
+    safety(number_customers);
+    
+
+
+
+
     //destroys the initialized mutex
     pthread_mutex_destroy(&lock);
+
+
     free(maximum);
+    free(allocation);
+    free(need);
     return 0;
 }
